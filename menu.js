6f4d7a5270abca228f0c1332626efd47bdc3ff70
alaaeldin989 <145039473+alaaeldin989@users.noreply.github.com>
2023-10-20 14:44:18 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,22 @@
+let red = document.getElementById('red')
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+
+}
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    
+}
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    
+}
+red.onclick =function(){
+    red.style.background = 'red';
+}
